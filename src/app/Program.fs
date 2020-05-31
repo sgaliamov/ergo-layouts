8dc427f3d27fdc5cf372b28d8adcadf35edf766a
s. galiamov <salavat.galiamov@neocles.io>
2020-05-31 15:20:45 +0200

    Init

---
+++ src/app/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.IO
+
+// load csv file
+// render csv file
+
+let readFile name =
+    let stream = File.OpenText name
+    seq {
+        while not stream.EndOfStream do yield stream.Read()
+    }
+
+let loadCsv path =
+    File.ReadAllText path
+
+[<EntryPoint>]
+let main argv =
+    let chars  = readFile "C:\\Users\\sgaliamov\\projects\\personal\\git.md"
+    for c in chars do
+        Console.Write c
+    Console.ReadKey()
+    |> ignore
+    0 // return an integer exit code
