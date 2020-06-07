3e88a984c1be3bed8031cb06a6cd586dc87a0aec
s. galiamov <sgalyamov@gmail.com>
2020-06-07 16:38:32 +0200

    iterate files

---
+++ src/app/Modules/Main.fs
@@ -0,0 +1,27 @@
+﻿module Main
+
+open System.IO
+open System.Threading
+
+let private streamFile path =
+    let stream = File.OpenText path
+    seq {
+        while not stream.EndOfStream do yield stream.Read()
+    }
+
+let private iterateFiles path =
+    seq { yield! Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories) }
+
+let private calculateFile (filePath: string) (token: CancellationToken): string =
+    ""
+
+let private isCancelled (token: CancellationToken) = token.IsCancellationRequested
+
+let calculate path (token: CancellationToken) =
+    let files = 
+        iterateFiles path
+        |> Seq.where (fun _ -> isCancelled token)
+        |> Seq.map calculateFile
+    for m in files do
+        m token
+    ()
--- src/app/Program.fs
+++ src/app/Program.fs
@@ -1,13 +1,13 @@
-﻿open System.CommandLine
+﻿open System
+open System.CommandLine
 open System.CommandLine.Invocation
-open MainModule
 open System.Threading
-open System
+open Main
 
 let private handler path = 
     printfn "Press any key to exit..."
     use cts = new CancellationTokenSource()
-    processFolders path cts.Token // todo: find a way to be pure async
+    calculate path cts.Token // todo: find a way to be pure async
     Console.ReadKey false |> ignore
     cts.Cancel true
     printf "Done."
@@ -16,5 +16,5 @@ let private handler path =
 let main argv =
     let root = RootCommand()
     Option("--path") |> root.AddOption
-    root.Handler <- handler |> CommandHandler.Create 
+    root.Handler <- handler |> CommandHandler.Create
     root.Invoke argv
