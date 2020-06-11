f036bb7ff03100f5e0b420c458d03cb9427e3fef
s. galiamov <sgalyamov@gmail.com>
2020-06-11 23:18:04 +0200

    fixed cancellation

--- src/app/Program.fs
+++ src/app/Program.fs
@@ -2,17 +2,18 @@
 open System.Threading
 open Main
 
-let private handler path search = 
+let private handler path search =
     printfn "Press any key to exit..."
     use cts = new CancellationTokenSource()
     let task = calculate path search cts.Token |> Async.StartAsTask
     Console.ReadKey true |> ignore
     cts.Cancel true
+    // todo: better awaiting
     task.GetAwaiter().GetResult()
 
 [<EntryPoint>]
 let main argv =
     match argv with
-    | [| first; second |] -> handler first second 
+    | [| first; second |] -> handler first second
     | _ -> printfn "Wrong input."
     0
--- src/app/modules/Main.fs
+++ src/app/modules/Main.fs
@@ -6,6 +6,7 @@ open System.Collections.Concurrent
 open System.Collections.Generic
 open System.Text
 open Calculations
+open System.Threading
 
 type StringBuilder with
     member sb.appendLine (key, value, total) =
@@ -14,7 +15,7 @@ type StringBuilder with
             | 0 -> 0.0
             | _ -> (float x) / (float y)
         let getValue value total = 100.0 * div value total
-        sb.AppendFormat("{0} : {1:0.###}\n", key, (getValue value total))
+        sb.AppendFormat("{0} : {1:0.####}\n", key, (getValue value total))
 
     member sb.appendLines<'T> (pairs: seq<KeyValuePair<'T, int>>) total =
         pairs
@@ -26,7 +27,7 @@ type StringBuilder with
 let private print (digraphs: ConcurrentDictionary<string, int>, letters: ConcurrentDictionary<char, int>, allChars: ConcurrentDictionary<char, int>) =
     let totalLetters = Seq.sum letters.Values
     let totalChars = Seq.sum allChars.Values
-    let symbolsOnly = allChars |> Seq.filter (fun x -> Char.IsSymbol(x.Key))
+    let symbolsOnly = allChars |> Seq.filter (fun x -> Char.IsPunctuation(x.Key))
     let totalDigraphs = Seq.sum digraphs.Values
     StringBuilder()
         .AppendFormat("Letters: {0}\n", totalLetters)
@@ -37,10 +38,10 @@ let private print (digraphs: ConcurrentDictionary<string, int>, letters: Concurr
         .Append(digraphs, totalDigraphs)
     |> Console.WriteLine
 
-let calculate path search token = async {
+let calculate path search (token: CancellationToken) = async {
     let seed = (ConcurrentDictionary<string, int>(), ConcurrentDictionary<char, int>(), ConcurrentDictionary<char, int>())
     Directory.EnumerateFiles(path, search, SearchOption.AllDirectories)
-    |> Seq.map calculateFile // todo: can run in pararllel
-    |> Seq.map (fun m -> m token)
+    |> Seq.filter (fun _ -> not token.IsCancellationRequested)
+    |> Seq.map (fun filePath -> calculateFile filePath token) // todo: can run in pararllel
     |> Seq.fold aggregator seed
     |> print }
