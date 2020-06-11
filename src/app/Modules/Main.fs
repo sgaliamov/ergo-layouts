cf9d2d29c266a0e7b8d2bfef337e16cd8e6fbd51
s. galiamov <sgalyamov@gmail.com>
2020-06-11 18:12:36 +0200

    used extension

--- src/app/Modules/Main.fs
+++ src/app/Modules/Main.fs
@@ -7,32 +7,31 @@ open System.Collections.Generic
 open System.Text
 open Calculations
 
-let private div x y =
-    match y with
-    | 0 -> 0.0
-    | _ -> (float x) / (float y)
-
-let private getValue value total = 100.0 * div value total
-
 type StringBuilder with
-    static member AppendLine (sb: StringBuilder, key, value, total) = 
+    member sb.appendLine (key, value, total) =
+        let div x y =
+            match y with
+            | 0 -> 0.0
+            | _ -> (float x) / (float y)
+        let getValue value total = 100.0 * div value total
         sb.AppendFormat("{0} : {1:0.###}\n", key, (getValue value total))
-    member sb.AppendLines<'T> (pairs: seq<KeyValuePair<'T, int>>) total =
+
+    member sb.appendLines<'T> (pairs: seq<KeyValuePair<'T, int>>) total =
         pairs
         |> Seq.sortByDescending (fun pair -> pair.Value)
-        |> Seq.fold (fun (sb, total) pair -> 
-            StringBuilder.AppendLine(sb, pair.Key, pair.Value, total)) (sb, total)
+        |> Seq.fold (fun (sb: StringBuilder) pair -> sb.appendLine(pair.Key, pair.Value, total)) sb
+
+    member sb.Append (pairs, total) = sb.appendLines pairs total
 
 let private print (digraphs: ConcurrentDictionary<string, int>, letters: ConcurrentDictionary<char, int>) =
     let totalLetters = Seq.sum letters.Values
     let totalDigraphs = Seq.sum digraphs.Values
-    let sb = StringBuilder().AppendFormat("Letters: {0}\n", totalLetters)
-
-    sb.AppendLines (letters, totalLetters)
-      .AppendFormat("\nDigraphs {0}:\n", totalDigraphs)
-    sb.AppendLines digraphs totalDigraphs
-
-    Console.WriteLine(sb)
+    StringBuilder()
+        .AppendFormat("Letters: {0}\n", totalLetters)
+        .Append(letters, totalLetters)
+        .AppendFormat("\nDigraphs {0}:\n", totalDigraphs)
+        .Append(digraphs, totalDigraphs)
+    |> Console.WriteLine
 
 let calculate path search token = async {
     let seed = (ConcurrentDictionary<string, int>(), ConcurrentDictionary<char, int>())
