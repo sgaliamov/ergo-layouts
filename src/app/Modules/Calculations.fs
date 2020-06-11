6e223476b15f2d924dc64987f507b8332e629ee4
s. galiamov <salavat.galiamov@neocles.io>
2020-06-11 14:50:01 +0200

    added calculation module

---
+++ src/app/Modules/Calculations.fs
@@ -0,0 +1,49 @@
+﻿module Calculations
+
+open System
+open System.IO
+open System.Threading
+open System.Collections.Concurrent
+
+open Utilities
+
+let toString (a, b) = String([| a; b |])
+       
+let countLetters line =
+    line 
+    |> Seq.countBy id
+
+let countDigraphs line =
+    line
+    |> Seq.filter Char.IsLetter
+    |> Seq.pairwise
+    |> Seq.map toString
+    |> Seq.countBy id
+
+let yieldLines (stream: StreamReader) (token: CancellationToken) = seq {
+    while not stream.EndOfStream && not token.IsCancellationRequested do
+        yield stream.ReadLine().ToLowerInvariant() } // todo: use async
+
+let collect line =
+    let folder result (key, count) = addOrUpdate result key count (+)
+    let letters =
+        line
+        |> countLetters
+        |> Seq.fold folder (ConcurrentDictionary<char, int>())
+    let digraphs =
+        line
+        |> countDigraphs
+        |> Seq.fold folder (ConcurrentDictionary<string, int>())
+    (digraphs, letters)
+
+let aggregator (digraphs, letters) (fromDigraphs, fromLetters) =
+    let resultDigraphs = sumValues fromDigraphs digraphs
+    let resultLetters = sumValues fromLetters letters
+    (resultDigraphs, resultLetters)
+
+let calculateFile filePath token =
+    use stream = File.OpenText filePath
+    let seed = (ConcurrentDictionary<string, int>(), ConcurrentDictionary<char, int>())
+    yieldLines stream token
+    |> Seq.map collect
+    |> Seq.fold aggregator seed
--- src/app/Modules/Main.fs
+++ src/app/Modules/Main.fs
@@ -1,74 +1,34 @@
 ﻿module Main
 
-open System
 open System.IO
-open System.Threading
 open System.Collections.Concurrent
-open Utilities
+open Calculations
 
-    module private Private =
-        let toString (a, b) = String([| a; b |])
-        
-        let countLetters line =
-            line 
-            |> Seq.countBy id
+let private print (digraphs: ConcurrentDictionary<string, int>, letters: ConcurrentDictionary<char, int>) =
+    let total = Seq.sum letters.Values
+    let div x y =
+        match y with
+        | 0 -> 0.0
+        | _ -> (float x) / (float y)
+    let getValue value total = 100.0 * div value total
+    printfn "Total: %d\n" total
+            
+    printfn "Letters:"
+    let printLetters key value = printfn "%c: %.3f" key value
+    letters
+    |> Seq.sortByDescending (fun pair -> pair.Value)
+    |> Seq.iter (fun pair -> printLetters pair.Key (getValue pair.Value total))
+            
+    printfn "\nDigraphs:"
+    let printDigraphs key value = printfn "%s: %.3f" key value
+    digraphs
+    |> Seq.sortByDescending (fun pair -> pair.Value)
+    |> Seq.iter (fun pair -> printDigraphs pair.Key (getValue pair.Value total))
 
-        let countDigraphs line =
-            line 
-            |> Seq.pairwise
-            |> Seq.map toString
-            |> Seq.countBy id
-
-        let yieldLines (stream: StreamReader) (token: CancellationToken) = seq {
-            while not stream.EndOfStream && not token.IsCancellationRequested do
-                yield stream.ReadLine() } // todo: use async
-
-        let collect line =
-            let folder result (key, count) = addOrUpdate result key count (+)
-            let letters =
-                line
-                |> countLetters
-                |> Seq.fold folder (ConcurrentDictionary<char, int>())
-            let digraphs =
-                line
-                |> countDigraphs
-                |> Seq.fold folder (ConcurrentDictionary<string, int>())
-            (digraphs, letters)
-
-        let aggregator (digraphs, letters) (fromDigraphs, fromLetters) =
-            let resultDigraphs = sumValues fromDigraphs digraphs
-            let resultLetters = sumValues fromLetters letters
-            (resultDigraphs, resultLetters)
-
-        let calculateFile filePath token =
-            use stream = File.OpenText filePath
-            let seed = (ConcurrentDictionary<string, int>(), ConcurrentDictionary<char, int>())
-            yieldLines stream token
-            |> Seq.map collect
-            |> Seq.fold aggregator seed
-
-        let div x y =
-            match y with
-            | 0 -> 0.0
-            | _ -> (float x) / (float y)
-
-        let print (digraphs: ConcurrentDictionary<string, int>, letters: ConcurrentDictionary<char, int>) =
-            let total = Seq.sum letters.Values
-            let printDigraphs key value total = printfn "%s: %.3f" key (div value total)
-            let printLetters key value total = printfn "%c: %.3f" key (div value total)
-            printfn "Letters:"
-            letters
-            |> Seq.iter (fun pair -> printLetters pair.Key pair.Value total)
-            printfn "Digraphs:"
-            digraphs
-            |> Seq.iter (fun pair -> printDigraphs pair.Key pair.Value total)
-
-    open Private
-
-    let calculate path search token = async {
-        let seed = (ConcurrentDictionary<string, int>(), ConcurrentDictionary<char, int>())
-        Directory.EnumerateFiles(path, search, SearchOption.AllDirectories)
-        |> Seq.map calculateFile // todo: can run in pararllel
-        |> Seq.map (fun m -> m token)
-        |> Seq.fold aggregator seed
-        |> print }
+let calculate path search token = async {
+    let seed = (ConcurrentDictionary<string, int>(), ConcurrentDictionary<char, int>())
+    Directory.EnumerateFiles(path, search, SearchOption.AllDirectories)
+    |> Seq.map calculateFile // todo: can run in pararllel
+    |> Seq.map (fun m -> m token)
+    |> Seq.fold aggregator seed
+    |> print }
