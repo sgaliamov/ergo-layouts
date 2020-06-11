efdce6618e49678ce36a754f6a3245f7906038bf
s. galiamov <sgalyamov@gmail.com>
2020-06-11 21:54:25 +0200

    wait for result

--- src/app/Program.fs
+++ src/app/Program.fs
@@ -5,13 +5,14 @@ open Main
 let private handler path search = 
     printfn "Press any key to exit..."
     use cts = new CancellationTokenSource()
-    calculate path search cts.Token |> Async.Start
-    Console.ReadKey false |> ignore
+    let task = calculate path search cts.Token |> Async.StartAsTask
+    Console.ReadKey true |> ignore
     cts.Cancel true
+    task.GetAwaiter().GetResult()
 
 [<EntryPoint>]
 let main argv =
     match argv with
-    | [| first; second |] -> handler first second
+    | [| first; second |] -> handler first second 
     | _ -> printfn "Wrong input."
     0
