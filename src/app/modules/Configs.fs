7f6624b07e4e53bfc58e6cfbee5f6424b7a05b28
s. galiamov <sgalyamov@gmail.com>
2020-06-13 23:36:41 +0200

    load statistics

--- src/app/modules/Configs.fs
+++ src/app/modules/Configs.fs
@@ -4,8 +4,10 @@ open FSharp.Data
 open System.IO
 
 type private Config = JsonProvider<"./data/config.json">
+type private Stats = JsonProvider<"./data/statistics.json">
 
 let config = Config.GetSample()
+let statistics = Stats.GetSample()
 
-let private loadCsv path =
+let loadLayout path =
     File.ReadAllText path
