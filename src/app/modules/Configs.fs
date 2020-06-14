d99020b444af56b8e0b9b53d492e9eb827fce950
s. galiamov <sgalyamov@gmail.com>
2020-06-14 02:01:07 +0200

    configs

--- src/app/modules/Configs.fs
+++ src/app/modules/Configs.fs
@@ -1,13 +1,38 @@
 ﻿module Configs
 
 open FSharp.Data
+open FSharp.Data.JsonExtensions
 open System.IO
+open Calculations
+open System
+open System.Linq
 
 type private Config = JsonProvider<"./data/config.json">
 type private Stats = JsonProvider<"./data/statistics.json">
 
 let config = Config.GetSample()
 let statistics = Stats.GetSample()
+
+//let map =
+//    statistics.JsonValue
+//        .GetProperty("digraphs")
+//        .AsArray()
+//    |> Seq.map (fun x -> x.as)
+
+
+//for i in map do  
+//    i.
+
+
+
+//let isEnough (state: State) set =
+//    state.digraphs.Keys
+//    |> Seq.map (fun key ->
+//        let delta = int statistics.Precision * state.totalDigraphs
+//        let goal = statistics.Digraphs..[key] * state.totalDigraphs
+//        let current = state.digraphs.[key]
+//        Math.Abs(goal - current)
+//    )
 
 let loadLayout path =
     File.ReadAllText path
