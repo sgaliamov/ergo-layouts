2521c6d40da4f7a72ddb489cc653d592bea30afe
s. galiamov <sgalyamov@gmail.com>
2020-06-14 20:01:49 +0200

    timer

--- src/app/Program.fs
+++ src/app/Program.fs
@@ -3,13 +3,15 @@ open System.Threading
 open Main
 
 let private handler path search =
-    printfn "Press any key to exit..."
+    let start = DateTime.UtcNow
+    printfn "Press any key to finish..."
     use cts = new CancellationTokenSource()
     let task = calculate path search cts |> Async.StartAsTask
     Console.ReadKey true |> ignore
     cts.Cancel true
     // todo: better awaiting
     task.GetAwaiter().GetResult()
+    printf "Time: %s." ((DateTime.UtcNow - start).ToString("c"))
 
 [<EntryPoint>]
 let main argv =
