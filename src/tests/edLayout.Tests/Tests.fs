11570648d8e97fb11a59b2d6a62ec6e01843b2d4
s. galiamov <sgalyamov@gmail.com>
2020-06-14 03:16:35 +0200

    tests

--- src/tests/edLayout.Tests/Tests.fs
+++ src/tests/edLayout.Tests/Tests.fs
@@ -1,9 +1,20 @@
 module Tests
 
+open System.Linq
 open System
 open Xunit
 open Configs
 
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+
+type private Stats = JsonProvider<"./statistics.json">
+
+let statistics = Stats.GetSample()
+
 [<Fact>]
 let ``My test`` () =
+    let a = statistics.Digraphs.JsonValue.Properties
+    let b = a.ToDictionary(new Func<(string*JsonValue),string>(fun (a,b) -> a))
+    let c = a.Select(fun (a, b: JsonValue) -> (a, b.AsFloat())).ToArray()
     Assert.True(true)
