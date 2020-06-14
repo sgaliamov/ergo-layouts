2960b4da6ef834d2f71a1f50465919b0c8845fc6
s. galiamov <sgalyamov@gmail.com>
2020-06-14 11:54:43 +0200

    cheking

--- src/app/modules/Configs.fs
+++ src/app/modules/Configs.fs
@@ -5,6 +5,8 @@ open FSharp.Data.JsonExtensions
 open System.IO
 open System.Linq
 open Calculations
+open System
+open System.Collections.Concurrent
 
 type private Config = JsonProvider<"./data/config.json">
 type private Stats = JsonProvider<"./data/statistics.json">
@@ -12,25 +14,32 @@ type private Stats = JsonProvider<"./data/statistics.json">
 let config = Config.GetSample()
 let statistics = Stats.GetSample()
 
-let digraphsMap =
-    statistics.Digraphs
-        .JsonValue.Properties
-        .Select(fun (a: string, b: JsonValue) -> (a, b.AsFloat()))
-        .ToDictionary(fun (a: string, _: float) -> a, fun (_: string, b: float) -> b)
+let jsonValueToMap jsonValues =
+    jsonValues
+    |> Seq.map (fun (a: string, b: JsonValue) -> (a.ToString(), b.AsFloat()))
+    |> Map.ofSeq
 
-let lettersMap =
-    statistics.Letters
-        .JsonValue.Properties
-        .ToDictionary(fun (a: string, _: JsonValue) -> a, fun (_: string, b: JsonValue) -> b.AsFloat())
+let digraphsStatistics = 
+    statistics.Digraphs.JsonValue.Properties
+    |> jsonValueToMap
 
-//let isEnough (state: State) set =
-//    state.digraphs.Keys
-//    |> Seq.map (fun key ->
-//        let delta = int statistics.Precision * state.totalDigraphs
-//        let goal = digraphsMap.[key] * state.totalDigraphs
-//        let current = state.digraphs.[key]
-//        Math.Abs(goal - current)
-//    )
+let lettersStatistics =
+    statistics.Letters.JsonValue.Properties
+    |> jsonValueToMap
+
+let check state stats total precision =
+    let delta = int(precision * float total)
+    let goal = int(stats * float total)
+    let current = state
+    current + delta >= goal
+
+let isEnough (state: State) set =
+    state.digraphs.Keys
+    |> Seq.map (fun key ->
+        let stats = digraphsStatistics.[key]
+        let current = state.digraphs.[key]
+        (key, check current stats state.totalDigraphs (float statistics.Precision))
+    )
 
 let loadLayout path =
     File.ReadAllText path
