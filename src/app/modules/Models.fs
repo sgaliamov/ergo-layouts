483957c2a8351ced3540ac4903d4b55f42506f6f
s. galiamov <sgalyamov@gmail.com>
2020-06-15 17:13:46 +0200

    created models

---
+++ src/app/modules/Models.fs
@@ -0,0 +1,18 @@
+﻿module Models
+
+open System.Collections.Concurrent
+
+type Char = char
+type Letter = char
+type Chars = ConcurrentDictionary<Char, int>
+type Letters = ConcurrentDictionary<Letter, int>
+type Digraph = string
+type Digraphs = ConcurrentDictionary<Digraph, int>
+
+type State =
+    { Letters: Letters
+      Digraphs: Digraphs
+      Chars: Chars
+      TotalLetters: int
+      TotalDigraphs: int
+      TotalChars: int }
