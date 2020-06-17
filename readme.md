17105f9a41de6e518517c43971cf2c9a1d263fc3
Salavat Galiamov <sgaliamov@users.noreply.github.com>
2020-06-17 21:11:14 +0200

    Used Reactive Extensions (#3)

--- readme.md
+++ readme.md
@@ -4,8 +4,10 @@ The utility to examine layouts for ErgoDox keyboard.
 
 ## Why?
 
-There are plenty tools already that estimate layouts. But they all have the fatal flaw: they based on standard keyboards.
+There are plenty tools already that estimate layouts. But they all have the one fatal flaw: they based on standard keyboards.
 
-In addition, authors of those layouts assume that you use fingers strictly vertically. But in my case it's not true. For exapmple, it's much more comfortable for me to use ring finger for P and Q instesad of pinky. I don't know who is mutant in this case.
+In addition, authors of those layouts assume that you use fingers strictly vertically. But in my case it's not work.
+For exapmple, it's much more comfortable for me to use ring finger for P and Q instesad of pinky.
+I don't know who is mutant in this case.
 
 Another reason, I wanted to try F# for some small project and scope of this assigment is perfect for F#.
--- src/app/modules/Main.fs
+++ src/app/modules/Main.fs
@@ -3,76 +3,103 @@
 open System
 open System.IO
 open System.Collections.Generic
+open System.Reactive.Subjects
+open System.Reactive.Linq
 open System.Text
 open System.Threading
 open Calculations
 open Configs
 open Models
 
-type StringBuilder with
-    member sb.AppendPair(key, value) = sb.AppendFormat("{0,-2} : {1,-10:0.###}", key, value)
+let private appendLines<'T> (pairs: seq<KeyValuePair<'T, int>>) total minValue (builder: StringBuilder) =
+    let appendPair (key, value) = builder.AppendFormat("{0,-2} : {1,-10:0.###}", key, value)
+    let getValue value =
+        let div x y =
+            match y with
+            | 0 -> 0.0
+            | _ -> (float x) / (float y)
+        100.0 * div value total
 
-    member sb.AppendLines<'T> (pairs: seq<KeyValuePair<'T, int>>) total filter =
-        let getValue value =
-            let div x y =
-                match y with
-                | 0 -> 0.0
-                | _ -> (float x) / (float y)
-            100.0 * div value total
-
-        pairs
-        |> Seq.map (fun pair ->
-            ({| Key = pair.Key
-                Value = getValue pair.Value |}))
-        |> Seq.filter (fun pair -> pair.Value >= filter)
-        |> Seq.sortByDescending (fun pair -> pair.Value)
-        |> Seq.fold (fun (sb: StringBuilder, i) pair ->
-            if i % settings.columns = 0 && i <> 0 then sb.AppendLine() |> ignore
-            (sb.AppendPair(pair.Key, pair.Value), i + 1)) (sb, 0)
-        |> ignore
-        sb
-
-    member sb.Append(pairs, total, filter) = sb.AppendLines pairs total filter
+    pairs
+    |> Seq.map (fun pair ->
+        ({| Key = pair.Key
+            Value = getValue pair.Value |}))
+    |> Seq.filter (fun pair -> pair.Value >= minValue)
+    |> Seq.sortByDescending (fun pair -> pair.Value)
+    |> Seq.fold (fun (sb: StringBuilder, i) pair ->
+        match i % settings.columns = 0 && i <> 0 with
+        | true -> sb.AppendLine(), 0
+        | _ -> appendPair(pair.Key, pair.Value), i + 1)
+        (builder, 0)
+    |> ignore
+    builder
 
 let calculate path search (cts: CancellationTokenSource) =
     async {
+        let spacer = new string(' ', Console.WindowWidth)
+
+        let appendValue (title: string) value (builder: StringBuilder) =
+            let format = sprintf "\n%s\n{0}: {1,-%d}\n" spacer (settings.columns * 15 - title.Length - 2)
+            builder.AppendFormat(format, title, value)
+        
         let yieldLines (token: CancellationToken) filePath =
             seq {
                 use stream = File.OpenText filePath
                 while not stream.EndOfStream
                       && not token.IsCancellationRequested do
-                    yield stream.ReadLine()
-            } // todo: use async
+                    yield stream.ReadLine() // todo: use async
+            }
 
-        let print state =
+        let formatMain state  (builder: StringBuilder) =
             let symbolsOnly =
                 state.Chars
                 |> Seq.filter (fun x -> Char.IsPunctuation(x.Key) || x.Key = ' ')
+            builder
+            |> (appendValue "Letters" state.TotalLetters)
+            |> (appendLines state.Letters state.TotalLetters 0.0)
+            |> (appendValue "Symbols from total" state.TotalChars)
+            |> (appendLines symbolsOnly state.TotalChars 0.0)
 
+        let formatState state =
+            Console.SetCursorPosition (0, Console.CursorTop)
             StringBuilder()
-                .AppendFormat("Letters: {0}\n", state.TotalLetters)
-                .Append(state.Letters, state.TotalLetters, 0.0)
-                .AppendFormat("\n\nSymbols from total: {0}\n", state.TotalChars)
-                .Append(symbolsOnly, state.TotalChars, 0.0)
-                .AppendFormat("\n\nDigraphs {0}:\n", state.TotalDigraphs)
-                .Append(state.Digraphs, state.TotalDigraphs, 0.05)
+            |> (appendValue "Digraphs" state.TotalDigraphs)
+            |> (appendLines state.Digraphs state.TotalDigraphs 0.05)
+            |> formatMain state
             |> Console.WriteLine
+
+
+        let onStateChanged state =
+            let initialPosition = (Console.CursorLeft, Console.CursorTop)
+            StringBuilder()
+            |> formatMain state
+            |> Console.WriteLine
+            Console.SetCursorPosition initialPosition
+
+        use stateChangedStream = new Subject<State>()
+        use subscription = stateChangedStream.Sample(TimeSpan.FromSeconds(0.500)).Subscribe onStateChanged
 
         let folder state next =
             let newState = aggregator state next
             let digraphsFinished = isFinished newState.Digraphs settings.digraphs newState.TotalDigraphs settings.precision
             let lettersFinished = isFinished newState.Letters settings.letters newState.TotalLetters settings.precision
-            if digraphsFinished && lettersFinished then cts.Cancel()
+            if digraphsFinished && lettersFinished then
+                Console.SetCursorPosition(0, Console.CursorTop)
+                Console.Write spacer
+                printf "\rCollected enough data."
+                subscription.Dispose()
+                cts.Cancel()
+            stateChangedStream.OnNext newState
             newState
 
         let start = DateTime.UtcNow
 
+         // todo: run in pararllel
         Directory.EnumerateFiles(path, search, SearchOption.AllDirectories)
         |> Seq.filter (fun _ -> not cts.IsCancellationRequested)
-        |> Seq.map (yieldLines cts.Token)
-        |> Seq.map calculateLines // todo: can run in pararllel
+        |> Seq.map ((yieldLines cts.Token) >> calculateLines)
         |> Seq.fold folder initialState
-        |> print
+        |> formatState
 
-        printf "\nTime: %s." ((DateTime.UtcNow - start).ToString("c"))
+        printf "\nTime: %s\n" ((DateTime.UtcNow - start).ToString("c"))
     }
