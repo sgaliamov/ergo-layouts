5b455ed6518c52af129f389b74c28c1355a86b7e
s. galiamov <sgalyamov@gmail.com>
2020-06-18 17:54:25 +0200

    added keyboard module

---
+++ src/app/modules/Keyboard.fs
@@ -0,0 +1,31 @@
+﻿module Keyboard
+
+open System.IO
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+open Configs
+open Models
+open Utilities
+
+type private Efforts = JsonProvider<"./data/efforts.json">
+let efforts = Efforts.GetSample()
+
+type private KeyboardJson = JsonProvider<"./data/qwerty.json">
+
+type Hand =
+    | Left = 'L'
+    | Right = 'R'
+
+type Key = Hand * byte
+
+type Keyboard =
+    { symbols: Map<Character.Char, Key option>
+      efforts: Map<Character.Char, float>}
+
+let load (path: string) =
+    let keyboard = KeyboardJson.Load(path)
+
+    //let left = keyboard.Left.JsonValue.Properties
+    //|> jsonValueToPairs JsonExtensions.AsString
+    //|> Seq.map 
+    ()
