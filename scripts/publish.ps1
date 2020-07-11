4ce532cf0e892644eb0818be3ff2607d80aead98
s. galiamov <sgalyamov@gmail.com>
2020-07-11 21:24:08 +0200

    moved files

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -5,3 +5,5 @@ Get-ChildItem -Include "bin", "obj", "publish" -Recurse | Remove-Item -Force -Re
 dotnet publish .\src\edLayout -o .\publish -c release -r win-x64 -p:PublishSingleFile=true
 
 Copy-Item .\layouts -Destination .\publish\layouts -Recurse
+
+Copy-Item .\layouts-good\*.* -Destination .\publish\layouts -Recurse
