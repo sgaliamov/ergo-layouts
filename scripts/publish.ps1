4c1c20ae3e4e0f3e88b20e6e35a968e064b2177d
s. galiamov <sgalyamov@gmail.com>
2020-07-12 13:22:39 +0200

    more penalty on same finger

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -4,8 +4,7 @@ Get-ChildItem -Include "bin", "obj", "publish" -Recurse | Remove-Item -Force -Re
 
 dotnet publish .\src\edLayout -o .\publish -c release -r win-x64 -p:PublishSingleFile=true
 
-# Copy-Item .\layouts -Destination .\publish\layouts -Recurse
-
 New-Item .\publish\layouts -ItemType Directory -Force | Out-Null
-
+Copy-Item .\layouts\*.* -Destination .\publish\layouts -Recurse
 Copy-Item .\layouts-good\*.* -Destination .\publish\layouts -Recurse
+Copy-Item .\layouts-bad\*.* -Destination .\publish\layouts -Recurse
