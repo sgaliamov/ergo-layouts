4a43115b5d0ddbc01708bf423758203a0f0e0bcc
s. galiamov <sgalyamov@gmail.com>
2020-07-12 04:03:36 +0200

    no message

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -4,6 +4,8 @@ Get-ChildItem -Include "bin", "obj", "publish" -Recurse | Remove-Item -Force -Re
 
 dotnet publish .\src\edLayout -o .\publish -c release -r win-x64 -p:PublishSingleFile=true
 
-Copy-Item .\layouts -Destination .\publish\layouts -Recurse
+# Copy-Item .\layouts -Destination .\publish\layouts -Recurse
+
+New-Item .\publish\layouts -ItemType Directory -Force | Out-Null
 
 Copy-Item .\layouts-good\*.* -Destination .\publish\layouts -Recurse
