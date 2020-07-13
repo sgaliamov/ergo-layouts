418d5182e7c534dd08f9b534d150594e7718a6ea
s. galiamov <sgalyamov@gmail.com>
2020-07-13 21:00:47 +0200

    changed efforts

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -1,3 +1,7 @@
+param (
+    [switch] $all
+)
+
 $ErrorActionPreference = "Stop"
 
 Get-ChildItem -Include "bin", "obj", "publish" -Recurse | Remove-Item -Force -Recurse
@@ -5,6 +9,9 @@ Get-ChildItem -Include "bin", "obj", "publish" -Recurse | Remove-Item -Force -Re
 dotnet publish .\src\edLayout -o .\publish -c release -r win-x64 -p:PublishSingleFile=true
 
 New-Item .\publish\layouts -ItemType Directory -Force | Out-Null
-# Copy-Item .\layouts\*.* -Destination .\publish\layouts -Recurse
 Copy-Item .\layouts-good\*.* -Destination .\publish\layouts -Recurse
-# Copy-Item .\layouts-bad\*.* -Destination .\publish\layouts -Recurse
+
+if ($all) {
+    Copy-Item .\layouts\*.* -Destination .\publish\layouts -Recurse
+    Copy-Item .\layouts-bad\*.* -Destination .\publish\layouts -Recurse
+}
