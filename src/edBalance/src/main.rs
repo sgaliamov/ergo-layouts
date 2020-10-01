d7d60d291a97feed152502d31042e4cb3f3a6059
s. galiamov <sgalyamov@gmail.com>
2020-10-01 18:14:02 +0200

    added rust project

---
+++ src/edBalance/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // build left group
+    // right group is empty
+    // 1) get biggest/smallest pair and place it to the right group
+    // 2) find a pair to move to the right group that will give biggest result
+    // iterate letters to find which will give biggest sum of both groups
+        // calculate value of the left group without current letter
+        // calculate right groups with new letter
+        // when move joined letters move them all (zxcv, th?, er?)
+        // select maximum combination
+    // print maximized groups
+    // continue till left group has 11 letters (because rest 4 can be used for punctuation keys)
+    println!("Hello, world!");
+}
